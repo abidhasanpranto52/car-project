1bf63dcb5938363e1b932625e244b4874c64a623
abidhasanpranto52 <abidhasanpranto52@gmail.com>
2023-05-20 18:25:35 +0600

    load all toys data

--- src/Pages/All Toys/AllTouysInfo.jsx
+++ src/Pages/All Toys/AllTouysInfo.jsx
@@ -1,28 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const AllTouysInfo = () => {
-    return (
-        <div>
-            {/* row 1 */}
-            <tr>
-                <td>
-                  <div className="flex items-center space-x-3">
-                    <div>
-                      <div className="font-bold">Hart Hagerty</div>
-                      <div className="text-sm opacity-50">United States</div>
-                    </div>
-                  </div>
-                </td>
-                <td>Zemlak, Daniel and Leannon</td>
-                <td>Purple</td>
-                <td>$20</td>
-                <td>Purple</td>
-                <th>
-                  <button className="btn btn-outline btn-xs">details</button>
-                </th>
-              </tr>
+const AllTouysInfo = ({toy}) => {
+  return (
+    <tr>
+      <td>
+        <div className="flex items-center space-x-3">
+          <div>
+            <div className="font-bold">Hart Hagerty</div>
+            <div className="text-sm opacity-50">United States</div>
+          </div>
         </div>
-    );
+      </td>
+      <td>Zemlak, Daniel and Leannon</td>
+      <td>Purple</td>
+      <td>$20</td>
+      <td>Purple</td>
+      <th>
+        <button className="btn btn-outline btn-xs">details</button>
+      </th>
+    </tr>
+  );
 };
 
 export default AllTouysInfo;
--- src/Pages/All Toys/AllToys.jsx
+++ src/Pages/All Toys/AllToys.jsx
@@ -1,42 +1,72 @@
-
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import AllTouysInfo from "./AllTouysInfo";
 
 const AllToys = () => {
+  const { user } = useContext(AuthContext);
+  const [toys, setToys] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
-  
+  const url = "http://localhost:5000/alltoys";
+  useEffect(() => {
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setToys(data));
+  }, [url]);
+
+  useEffect(() => {
+    // Filter toys based on search term
+    const results = toys.filter((toy) =>
+      toy.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(results);
+  }, [searchTerm, toys]);
 
   return (
     <div>
-      <h2>All Toys</h2>
-      <input
-        type="text"
-        placeholder="Search by toy name..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        className="w-2/3 my-3 mx-auto border-2 rounded text-center"
-      />
-
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <div className="overflow-x-auto w-full">
-          <table className="table w-full">
-            {/* head */}
-            <thead>
-              <tr>
-                <th>Seller</th>
-                <th>Toy Name</th>
-                <th>Sub-category</th>
-                <th>Price</th>
-                <th>Available Quantity</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              
-            </tbody>
-          </table>
+      <div>
+        <h1 className="text-center text-2xl">My toys : {toys.length}</h1>
+        <input
+          type="text"
+          placeholder="Search by toy name..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="w-2/3 my-3 mx-auto border-2 rounded text-center"
+        />
+        <div>
+          <div className="overflow-x-auto w-full">
+            <table className="table w-full">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>Seller</th>
+                  <th>Toy Name</th>
+                  <th>Sub-category</th>
+                  <th>Price</th>
+                  <th>Available Quantity</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {toys.map((toy) => (
+                  <AllTouysInfo
+                    key={toy._id}
+                    toy={toy}
+                    // handleDelete={handleDelete}
+                    // handleBookingConfirm={handleBookingConfirm}
+                  ></AllTouysInfo>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
